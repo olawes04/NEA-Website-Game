1a234dedbff2c3e7f699034b729c25238a3512c2
Oliver Lawes <terrasquid04@gmail.com>
2024-09-13 15:16:31 +0100

    Onclick now works, although doing so as it hits the top causes problems

--- start_site.js
+++ start_site.js
@@ -1,28 +1,68 @@
 var canvas = document.getElementById("game_window");
-var ctx = canvas.getContext("2d");
-var x=500
-var y=500
-var dx=2
-var dy=-2
+var sqt = canvas.getContext("2d");
+//Starting Position
+var x=100
+var y=100
+//Speed
+var dx=1
+var dy=-1
+//Starting Colours
+var red=2
+var green=124
+var blue=34
+var colour="rgb("+red+","+green+","+blue+")"
+//Rate of Colour Change, breaks if greater than 1
+var redChange=1
+var greenChange=1
+var blueChange=1
 
+//Draws the square
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(x, y, 100, 0, Math.PI * 2);
-    ctx.fillStyle = "#000000";
-    ctx.fill();
-    ctx.closePath();
+  //Clears the square's "tail"
+    //sqt.clearRect(0, 0, canvas.width, canvas.height);
+    sqt.beginPath();
+    //Defines the square's size
+    sqt.rect(x, y, 5, 5);
+    sqt.fillStyle = colour;
+    sqt.fill();
+    sqt.closePath();
+    //actually makes the square move
     x+=dx
     y+=dy
-    // Check for canvas boundary collisions and reverse direction if necessary
-    if(x + dx > canvas.width - 10 || x + dx < 10) {
+    //actually changes the colour variables
+    red+=redChange
+    green+=greenChange  
+    blue+=blueChange
+    //Changes the rate of colour change if the colours have reached their maximum or minimum value
+    if(red==255){
+      redChange=-1;
+    }
+    if (red==0){
+      redChange=1;
+    }
+    if (green==255){
+      greenChange=-1;
+    }
+    if (green==0){
+      greenChange=1;
+    }
+    if (blue==255){
+      blueChange=-1;
+    }
+    if (blue==0){
+      blueChange=1;
+    }
+    // Check for if the square has hit the boundary and reverses the movement if it has
+    if(x> canvas.width || x< 5) {
         dx = -dx;
     }
-    if(y + dy > canvas.height - 10 || y + dy < 10) {
+    if(y> canvas.height|| y< 5) {
         dy = -dy;
     }
+  //Updates the colour
+  colour="rgb("+red+","+green+","+blue+")"
   }
-setInterval(draw, 10);
+setInterval(draw, 0);
 
 draw()
   //to draw the circle
