a321b7c7b54fb01b6e0e6d9c47c1ff672f3b9446
olawes04 <144459640+olawes04@users.noreply.github.com>
2024-09-20 15:45:43 +0100

    Gravity Enabled

--- start_site.js
+++ start_site.js
@@ -4,8 +4,8 @@ var sqt = canvas.getContext("2d");
 var x=100
 var y=100
 //Speed
-var dx=1
-var dy=-1
+var dx=1.0
+var dy=-1.0
 //Starting Colours
 var red=2
 var green=124
@@ -15,14 +15,15 @@ var colour="rgb("+red+","+green+","+blue+")"
 var redChange=1
 var greenChange=1
 var blueChange=1
+var onGround=false
 
 //Draws the square
 function draw() {
   //Clears the square's "tail"
-    //sqt.clearRect(0, 0, canvas.width, canvas.height);
+    sqt.clearRect(0, 0, canvas.width, canvas.height);
     sqt.beginPath();
     //Defines the square's size
-    sqt.rect(x, y, 5, 5);
+    sqt.rect(x, y, 50, 50);
     sqt.fillStyle = colour;
     sqt.fill();
     sqt.closePath();
@@ -53,16 +54,50 @@ function draw() {
       blueChange=1;
     }
     // Check for if the square has hit the boundary and reverses the movement if it has
-    if(x> canvas.width || x< 5) {
+    if(x> canvas.width-50 || x< 0) {
         dx = -dx;
     }
-    if(y> canvas.height|| y< 5) {
-        dy = -dy;
+    if(y> canvas.height-50) {
+        dy = 0;
+        onGround=true
     }
+    if (y>canvas.height-51){
+      onGround=false
+    }
+    if (y> canvas.height-49){
+      y=canvas.height-50;
+    }
+    if (onGround==false){
+      dy+=0.02;
+      console.log(dy);
+    }
+
+    if (y< 0) {
+      y=0;
+      console.log(dy);
+      onGround=false;
+
+    }
+
   //Updates the colour
   colour="rgb("+red+","+green+","+blue+")"
-  }
+ }
+
 setInterval(draw, 0);
 
 draw()
   //to draw the circle
+
+function down(){
+  console.log("If you are reading this I have died at sea");
+}
+
+function movementUp(){
+  dy-=3
+  
+  //setTimeout(down,500);
+}
+
+// canvas.addEventListener("keydown", (d)=>{
+//   console.log("")
+// })
